590219fe727e50bfe4ca8c6899f89590abf885f1
Erick Pinzon <erickpinzonhuerta@gmail.com>
2020-05-22 15:35:45 -0500

    Fase 3
    Fase 3 con bd

--- views/index.view.php
+++ views/index.view.php
@@ -1,46 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-	<title>Proyecto De CSS Galería Pinzon Huerta</title>
+<meta charset="UTF-8">
+<!-- Agregamos nuestro archivo de estiulos CSS-->
+<link rel="stylesheet" href="css/estilos.css">
+<title>Proyecto De CSS Galería Hernández Salvador</title>
 </head>
 <body>
-	<header>
-		<div class="contenedor">
-			<h1 class="titulo">Galeria de mis Relfexiones 2020</h1>
-		</div>
-	</header>
+<header>
+<div class="contenedor">
+<h1 class="titulo">Galería de Mis Reflexiones del 2020</h1>
+</div>
+</header>
 
-	<section class="fotos">
-		<div class="contenedor">
-			<div class="thumb">
-				<a href="#">
-					<img src="fotos/1.jpg" alt="">
-				</a>
-			</div>
 
-			<div class="thumb">
-				<a href="#">
-					<img src="fotos/5.jpg" alt="">
-				</a>
-			</div>
+<section class="fotos">
+<div class="contenedor">
+<?php foreach($fotos as $foto):?>
+<div class="thumb">
+<a href="foto.php?id=<?php echo $foto['id']; ?>">
+<img src="fotos/<?php echo $foto['imagen'] ?>" alt="">
+</a>
+</div>
+<?php endforeach;?>
 
-			<div class="thumb">
-				<a href="#">
-					<img src="fotos/6.jpg" alt="">
-				</a>
-			</div>
+<!-- En este apartado agregamos una paginación para mostrar solo una cantidad de imagnenes por pagina-->
+<div class="paginacion">
+<?php if ($pagina_actual > 1): ?>
+<a href="index.php?p=<?php echo $pagina_actual - 1; ?>" class="izquierda"> Pagina Anterior</a>
+<?php endif ?>
 
-			<div class="paginacion">
-				<a href="#" class="izquierda">Pagina Anterior</a>
-				<a href="#" class="derecha">Pagina Siguiente</a>
-			</div>
-		</div>
-	</section>
+<?php if ($total_paginas != $pagina_actual): ?>
+<a href="index.php?p=<?php echo $pagina_actual + 1; ?>" class="derecha">Pagina Siguiente </a>
+<?php endif ?>
+</div>
 
-	<footer>
-		<p class="copyright">Galeria Creada por Erick Pinzon</p>
-	</footer>
+
+</div>
+
+
+</section>
+<!-- Se agrega la etiqueta de pie de pagina para crear Cambia por tus datos-->
+<footer>
+<p class="copyright">Galeria creada por Algalan Murillo Diego Alexis y Pinzon Huerta Erick</p>
+</footer>
 </body>
 </html>
